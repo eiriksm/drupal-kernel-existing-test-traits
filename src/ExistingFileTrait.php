c6c35b72c02044d19a10b9b57d8399210ce90b75
Eirik S. Morland <eirik@morland.no>
2024-01-02 21:54:08 +0100

    First

---
+++ src/ExistingFileTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace eiriksm\KernelExistingTestTraits;
+
+/**
+ * Trait relating to fields and field config.
+ */
+trait KernelExistingTestTraits {
+
+  /**
+   * Create a field from existing config.
+   */
+  public function createFieldFromConfig($field_name) {
+
+  }
+
+}
