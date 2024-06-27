008f595b8cea551b378784204aac9ac62bfea61e
Eirik S. Morland <eirik@morland.no>
2024-06-27 14:18:38 +0200

    Make sure we find term bundles

--- src/ExistingBundleTrait.php
+++ src/ExistingBundleTrait.php
@@ -13,13 +13,18 @@ trait ExistingBundleTrait {
    */
   public function createBundleFromConfig(string $entity_type, string $bundle) {
     $this->requireConfigAwareImplementation();
+    // Find the bundle entity type.
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $entity_definition = $entity_type_manager->getDefinition($entity_type);
     // Get the YML file based on the config dir and the entity type and bundle.
     $config_dir = $this->getConfigDir();
     $bundle_config_file = $config_dir . "/$entity_type.type.$bundle.yml";
+    if ($entity_type === 'taxonomy_term') {
+      // This is a specific case where we know the bundle file will look like
+      // this.
+      $bundle_config_file = $config_dir . "/taxonomy.vocabulary.$bundle.yml";
+    }
     $data = $this->getDataFromYmlFile($bundle_config_file);
-    // Find the bundle entity type.
-    $entity_type_manager = \Drupal::entityTypeManager();
-    $entity_definition = $entity_type_manager->getDefinition('node');
     $bundle_entity_type = $entity_definition->getBundleEntityType();
     $storage = $entity_type_manager->getStorage($bundle_entity_type);
     $storage->create($data)->save();
